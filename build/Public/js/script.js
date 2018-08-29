caab3b4237b2a4a9aaa0f256871176c9c32671b9
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2018-08-28 23:27:39 -0600

    image gallery

--- build/Public/js/script.js
+++ build/Public/js/script.js
@@ -105,6 +105,61 @@ $('.js--nav-icon').click(function() {
     nav.slideToggle(500);
 })
 
+//// image gallery
 
+    var width = 600;
+    var animationSpedd = 2000;
+    var pause = 4000;
+    var currentSlide = 1;
+
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find('.slides');
+    var $slides = $slideContainer.find('.slide')
+
+    var interval;
+    function startSlider () {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpedd, function() {
+                currentSlide++;
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            })
+        }, pause)
+    };
+
+    function stopSlider () {
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+    startSlider()
+
+
+    var $slider2 = $('#slider2');
+    var $slideContainer2 = $slider2.find('.slides2');
+    var $slides2 = $slideContainer2.find('.slide2');
+    var currentSlide2 = 1
+
+    var interval2;
+    function startSlider2 () {
+        interval2 = setInterval(function() {
+            $slideContainer2.animate({'margin-left': '-='+width}, animationSpedd, function() {
+                currentSlide2++;
+                if (currentSlide2 === $slides2.length) {
+                    currentSlide2 = 1;
+                    $slideContainer2.css('margin-left', 0);
+                }
+            })
+        }, pause)
+    };
+
+    function stopSlider2 () {
+        clearInterval(interval2);
+    }
+
+    $slider2.on('mouseenter', stopSlider2).on('mouseleave', startSlider2);
+    startSlider2()
 
 });
